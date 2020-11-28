8966a96c39ce6e6b0c317eef637e57753ca6d28a
eikendev <raphael@eiken.dev>
2020-11-28 09:10:51 +0100

    Return nil when application cannot retrieved

--- authentication/context.go
+++ authentication/context.go
@@ -14,6 +14,7 @@ func GetApplication(ctx *gin.Context) *model.Application {
 	app, ok := ctx.MustGet("app").(*model.Application)
 	if app == nil || !ok {
 		ctx.AbortWithError(http.StatusInternalServerError, errors.New("an error occured while retrieving application from context"))
+		return nil
 	}
 
 	return app
