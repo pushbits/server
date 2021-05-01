567c8149686b35d9c6be949a1c94d33654cb0823
Cubicroot <github@cubicroot.xyz>
2021-05-01 21:34:21 +0200

    added formatting options

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.3.0 // indirect
 	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/gomarkdown/markdown v0.0.0-20210408062403-ad838ccf8cdd
 	github.com/google/go-cmp v0.5.0 // indirect
 	github.com/jinzhu/configor v1.2.1
 	github.com/json-iterator/go v1.1.10 // indirect
--- internal/dispatcher/notification.go
+++ internal/dispatcher/notification.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"strings"
 
+	"github.com/gomarkdown/markdown"
 	"github.com/pushbits/server/internal/model"
 )
 
@@ -16,10 +17,36 @@ func (d *Dispatcher) SendNotification(a *model.Application, n *model.Notificatio
 	plainTitle := strings.TrimSpace(n.Title)
 	plainMessage := strings.TrimSpace(n.Message)
 	escapedTitle := html.EscapeString(plainTitle)
-	escapedMessage := html.EscapeString(plainMessage)
+	message := html.EscapeString(plainMessage) // default to text/plain
+
+	if optionsDisplayRaw, ok := n.Extras["client::display"]; ok {
+		optionsDisplay, ok := optionsDisplayRaw.(map[string]interface{})
+		log.Printf("%s", optionsDisplay)
+
+		if ok {
+			if contentTypeRaw, ok := optionsDisplay["contentType"]; ok {
+				contentType := fmt.Sprintf("%v", contentTypeRaw)
+				log.Printf("Message content type: %s", contentType)
+
+				switch contentType {
+				case "html", "text/html":
+					message = plainMessage
+				case "markdown", "md", "text/md", "text/markdown":
+					message = string(markdown.ToHTML([]byte(plainMessage), nil, nil))
+				}
+			}
+		}
+	}
+
+	// TODO cubicroot: add colors for priority https://spec.matrix.org/unstable/client-server-api/#mroommessage-msgtypes
+	// maybe make this optional in the settings or so
+
+	// TODO cubicroot: check if we somehow can handle \n or other methods of line breaks
+
+	// TODO cubicroot: add docu
 
 	text := fmt.Sprintf("%s\n\n%s", plainTitle, plainMessage)
-	formattedText := fmt.Sprintf("<b>%s</b><br /><br />%s", escapedTitle, escapedMessage)
+	formattedText := fmt.Sprintf("<b>%s</b><br /><br />%s", escapedTitle, message)
 
 	_, err := d.client.SendFormattedText(a.MatrixID, text, formattedText)
 
