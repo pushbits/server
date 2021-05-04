de3f94fff3d823e7083fbb4fb8156ea5095a2808
Cubicroot <github@cubicroot.xyz>
2021-05-04 18:50:09 +0200

    remove testing

--- internal/dispatcher/notification.go
+++ internal/dispatcher/notification.go
@@ -24,8 +24,6 @@ func (d *Dispatcher) SendNotification(a *model.Application, n *model.Notificatio
 
 	_, err := d.client.SendFormattedText(a.MatrixID, text, formattedText)
 
-	//testing
-
 	return err
 }
 
